1faf5a5b2ceb19879a053d07c1d22e25a4e01c1b
Review <wednesday16331@gmail.com>
2019-04-17 13:44:56 +0700

    Create FeedsAndProfile VC

---
+++ Review/ViewController/FeedsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedsViewController.swift
+//  Review
+//
+//  Created by Donlaya on 17/4/2562 BE.
+//  Copyright © 2562 Donlaya. All rights reserved.
+//
+
+import UIKit
+
+class FeedsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
