26ffe79c013ba7fd8a3df08f3abfead510d2de4f
NanDonlaya <wednesday16331@gmail.com>
2019-04-19 16:06:45 +0700

    MainPage

--- Review/ViewController/AuthenViewController.swift
+++ Review/ViewController/AuthenViewController.swift
@@ -13,7 +13,7 @@ class AuthenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
--- Review/ViewController/MainViewController.swift
+++ Review/ViewController/MainViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.mainLabel.text = "MainPage"
     }
     
 
